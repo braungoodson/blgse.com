b1b04160bd9c1c6588fd7b97de5bd25a08632cc7
snake <snake@i7.local>
2015-05-28 09:54:58 -0400

    trash

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-  server = express(),
-  port = process.env.PORT || 30000,
-  staticRoot = __dirname;
-
-server.use('/', express.static(staticRoot));
-server.listen(port);
-console.log('http://localhost:'+port);
